7b63e55933346706909e7dd6075bcd59e608f47b
sauce-picante <105173634+sauce-picante@users.noreply.github.com>
2022-05-10 19:11:29 -0400

    Add files via upload

---
+++ NinetyThreeBenz/Driver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BenzChess
+{
+    /*
+     * This Driver class is used for communication with the GUI
+     * and the engine through the Universal Chess Interface 
+     * or "UCI".
+     * 
+     */
+
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            //Infinite loop for Engine-Interface communication
+            while (true)
+            {
+                //Commmands from the Universal Chess Interface to the engine
+                //.Split() breaks a delineated string into substrings, needed to split parameters for the "go" command
+                string command = Console.ReadLine().Split()[0];
+                switch (command)
+                {
+                    case "uci": //tells the engine to use the Universal Chess Interface
+                        Console.WriteLine("uciok");
+                        break;
+                    case "isready": //synchronizes the engine with the GUI
+                        Console.WriteLine("readyok");
+                        break;
+                    case "go": //start calculating the position that was setup with "position" command
+                        Console.WriteLine("bestmove e7e5"); //black king's pawn open
+                        break;
+                    default:
+                       // Debugger.Launch();
+                        break;
+
+                }
+            }
+
+
+        }
+    }
+}
---
+++ NinetyThreeBenz/Move.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenzChess
+{
+    public struct Move   
+    {
+        public byte FromIndex;
+        public byte ToIndex;
+        public Piece Promotion;
+
+        public Move(byte from, byte to, Piece promotion)
+        {
+            FromIndex = from;
+            ToIndex = to;
+            Promotion = promotion;
+        }
+
+        //method to generate a move given UCI move notation
+        //ex: e2e4, e7r5, e1g1, e7e8q
+        public Move(string moveNotation)
+        {
+            //parameter is split into to and from
+            // ex: e2e4 -> e2 is where the piece is coming from, e4 is where it is going
+            string fromSquare = moveNotation.Substring(0, 2);
+            string toSquare = moveNotation.Substring(2, 2);
+
+            FromIndex = Notation.ToSquare(fromSquare);
+            ToIndex = Notation.ToSquare(toSquare);
+
+            //ternary operator to determine if a pawn promotion is to take place
+            Promotion = (moveNotation.Length == 5) ? Notation.ToPiece(moveNotation[4]) : Piece.None;
+        }
+    }
+}
